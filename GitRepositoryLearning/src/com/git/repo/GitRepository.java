911417a67de555736df438ab437b156e9ac37387
Judith Aio <Judith Aio@DESKTOP-KSD455U>
2019-04-02 15:31:23 -0400

    Main Project

---
+++ GitRepositoryLearning/src/com/git/repo/GitRepository.java
@@ -0,0 +1,33 @@
+package com.git.repo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GitRepository {
+	
+
+	public static void main(String[] args) {
+
+
+        List<String> verticesposition2 = new ArrayList<String>();
+
+        verticesposition2.add("safdsfVertex");
+
+        verticesposition2.add("safdsfNamesfsd");
+     verticesposition2.add("notCONTAINS");
+        verticesposition2.add(" shoes");
+
+        for (String str5 : verticesposition2){
+
+            if((str5.contains("Vertex")||str5.contains("Name")||str5.contains("Transmittance")||str5.contains("shoes"))){
+
+                System.out.println(str5);
+
+       
+            }
+        }
+	
+	}
+	
+
+}
