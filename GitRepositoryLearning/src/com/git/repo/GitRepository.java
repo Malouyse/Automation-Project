30966b7733935ef970b483775a188ef6c2fcd5c8
Judith Aio <Judith Aio@DESKTOP-KSD455U>
2019-04-02 15:44:44 -0400

    1st Andy coding

--- GitRepositoryLearning/src/com/git/repo/GitRepository.java
+++ GitRepositoryLearning/src/com/git/repo/GitRepository.java
@@ -28,6 +28,7 @@ public class GitRepository {
                 int num3;
                 num3 = num1 + num2;
                 System.out.println(num3);
+                String hello = "I am new to gitHub";
                 
 
        
